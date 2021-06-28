dc4216cdfd2f47bfd5216604b30bb71cb1a2fae5
didinele <didinele.dev@gmail.com>
2021-06-28 16:32:54 +0000

    feat: basic packet handling and colors

--- libs/core/src/brokers.ts
+++ libs/core/src/brokers.ts
@@ -2,7 +2,8 @@ import type {
   GatewayDispatchEvents,
   GatewayDispatchPayload,
   APIApplicationCommandInteraction,
-  APIMessageComponentInteraction
+  APIMessageComponentInteraction,
+  Snowflake
 } from 'discord-api-types/v8';
 
 type SanitizedDiscordEvents = {
@@ -19,3 +20,34 @@ export interface DiscordInteractions {
   command: APIApplicationCommandInteraction;
   component: APIMessageComponentInteraction;
 }
+
+export enum LogTypes {
+  modAction
+}
+
+export interface LogBase<T extends LogTypes, D extends Record<string, any>> {
+  type: T;
+  data: D;
+}
+
+export enum ModAction {
+  warn,
+  strike,
+  mute,
+  unmute,
+  kick,
+  softban,
+  ban,
+  unban
+}
+
+export type ModActionLog = LogBase<LogTypes.modAction, {
+  mod_id: Snowflake;
+  target_id: Snowflake;
+  type: ModAction;
+  createdAt: Date;
+  expiresAt?: Date;
+  reason?: string;
+}>;
+
+export type Log = ModActionLog;
