54ba60196630bf200240273dc7a78f58970c2d62
didinele <didinele.dev@gmail.com>
2021-06-30 08:49:26 +0000

    refactor(interactions): split discord webhook into own microservice

--- services/api/src/index.ts
+++ services/api/src/index.ts
@@ -7,17 +7,12 @@ import { container } from 'tsyringe';
 import postgres from 'postgres';
 import { join as joinPath } from 'path';
 import { readdirRecurse } from '@gaius-bot/readdir';
-import { createAmqp, RoutingServer } from '@cordis/brokers';
 import * as controllers from './controllers';
 
 void (async () => {
   const config = initConfig();
   const logger = createLogger('API');
 
-  const { channel } = await createAmqp(config.amqpUrl);
-  const interactions = new RoutingServer(channel);
-
-  container.register(RoutingServer, { useValue: interactions });
   container.register(kLogger, { useValue: logger });
   container.register(
     kSql, {
@@ -36,7 +31,6 @@ void (async () => {
   container.register(TokenManager, { useClass: TokenManager });
 
   await initApp(app, readdirRecurse(joinPath(__dirname, 'routes'), { fileExtension: 'js' }));
-  await interactions.init({ name: 'interactions', topicBased: false });
 
   app.listen(3001, () => logger.info({ topic: 'INIT' }, 'Listening to requests on port 3001'));
 })();
