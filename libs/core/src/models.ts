f2309c8260b4886234bf05d813309b40789625a5
didinele <didinele.dev@gmail.com>
2021-07-11 11:44:02 +0000

    chore: add self assignable roles prompt field in guild settings

--- libs/core/src/models.ts
+++ libs/core/src/models.ts
@@ -13,6 +13,7 @@ export interface GuildSettings {
   use_url_filters: UseFilterMode;
   use_file_filters: UseFilterMode;
   mod_action_log_channel: Snowflake | null;
+  assignable_roles_prompt: string | null;
 }
 
 export enum StrikePunishmentAction {
