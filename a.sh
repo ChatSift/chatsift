b9ccb9569e801e8ad2477bcc808d738f6c52dc34
didinele <didinele.dev@gmail.com>
2021-07-25 15:17:54 +0000

    refactor(docker): significantly increase build speeds for no lib code changes

--- a.sh
+++ a.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+[ $2 == build ] && docker build -t automoderator:base -f ./docker/base/Dockerfile .
 docker-compose -f docker-compose.yml -f docker-compose.$1.yml -f docker-compose.config.yml ${@%$1}
