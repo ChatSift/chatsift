f7241fd2a265b02009cce1848994ee37b00808a4
didinele <didinele.dev@gmail.com>
2021-07-25 15:36:28 +0000

    refactor: use compose instead of building in the bash script

--- a.sh
+++ a.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-[ $2 == build ] && docker build -t automoderator:base -f ./docker/base/Dockerfile .
 docker-compose -f docker-compose.yml -f docker-compose.$1.yml -f docker-compose.config.yml ${@%$1}
