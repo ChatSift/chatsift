5020c38b1924bec852c2348a4510296975b60416
didinele <didinele.dev@gmail.com>
2021-09-02 12:35:06 +0300

    feat(api): PostFiltersUrlsBulkRoute

---
+++ services/api/src/routes/api/v1/filters/urls/bulk/post.ts
@@ -0,0 +1,60 @@
+import { ApiPutFiltersUrlsBody, MaliciousUrl, MaliciousUrlCategory } from '@automoderator/core';
+import { kSql } from '@automoderator/injection';
+import { jsonParser, Route, thirdPartyAuth, validate } from '@automoderator/rest';
+import * as Joi from 'joi';
+import type { Request, Response } from 'polka';
+import type { Sql } from 'postgres';
+import { inject, injectable } from 'tsyringe';
+
+@injectable()
+export default class PostFiltersUrlsBulkRoute extends Route {
+  public override readonly middleware = [
+    thirdPartyAuth(),
+    jsonParser(),
+    validate(
+      Joi
+        .object()
+        .keys({
+          url: Joi.string().required(),
+          category: Joi.number()
+            .min(MaliciousUrlCategory.malicious)
+            .max(MaliciousUrlCategory.urlShortner)
+            .required()
+        })
+        .required(),
+      'body'
+    )
+  ];
+
+  public constructor(
+    @inject(kSql) public readonly sql: Sql<{}>
+  ) {
+    super();
+  }
+
+  public async handle(req: Request, res: Response) {
+    const urls = (req.body as ApiPutFiltersUrlsBody).map(domain => ({ ...domain, admin: req.user!.id }));
+
+    const data = await this.sql.begin(async sql => {
+      await sql`DELETE FROM malicious_urls`;
+      const promises: Promise<MaliciousUrl>[] = [];
+
+      for (const url of urls) {
+        const promise = sql<[MaliciousUrl]>`
+          INSERT INTO malicious_urls (url, admin_id, category)
+          VALUES (${url.url}, ${url.admin}, ${url.category})
+          RETURNING *
+        `.then(rows => rows[0]);
+
+        promises.push(promise);
+      }
+
+      return Promise.all(promises);
+    });
+
+    res.statusCode = 200;
+    res.setHeader('content-type', 'application/json');
+
+    return res.end(JSON.stringify(data));
+  }
+}
