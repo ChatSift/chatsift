fe2049e21850a731bc686bda0d274cdff0782b92
didinele <didinele.dev@gmail.com>
2021-12-07 20:02:22 +0200

    fix(dash/GuildIcon): add placeholder icon for guilds that don't have one

--- services/dash/src/components/GuildIcon.tsx
+++ services/dash/src/components/GuildIcon.tsx
@@ -5,7 +5,7 @@ import type { Snowflake } from 'discord-api-types/v8';
 const MotionBox = dynamic(() => import('~/components/MotionBox'));
 
 const GuildIcon = ({ guild }: { guild?: { id: Snowflake; name: string; icon: string | null } }) =>
-  guild?.icon
+  guild
     ? (
       <MotionBox whileHover = {{ scale: 1.05, rotate: 5 }}
         whileTap = {{ scale: 0.95 }}
@@ -13,10 +13,9 @@ const GuildIcon = ({ guild }: { guild?: { id: Snowflake; name: string; icon: str
       >
         <Img rounded = "full"
           boxSize = "100px"
-          src = {`https://cdn.discordapp.com/icons/${guild.id}/${guild.icon}${
-            guild.icon.startsWith('a_') ? '.gif' : '.png'
-          }`}
-          alt = {guild.name}
+          src = {guild.icon
+            ? `https://cdn.discordapp.com/icons/${guild.id}/${guild.icon}${guild.icon.startsWith('a_') ? '.gif' : '.png'}`
+            : `https://cdn.discordapp.com/embed/avatars/${guild.id.split('').reduce((a, b) => a + parseInt(b, 10), 0) % 5}.png`}
         />
       </MotionBox>
     )
