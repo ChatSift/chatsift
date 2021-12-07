3e03efd1cf25e4c0abedc4a3c9efa6683328c424
didinele <didinele.dev@gmail.com>
2021-12-07 20:03:11 +0200

    fix(dash/Guilds): handle line wraps correctly

--- services/dash/src/components/Guilds.tsx
+++ services/dash/src/components/Guilds.tsx
@@ -1,11 +1,12 @@
 import dynamic from 'next/dynamic';
 import Link from 'next/link';
-import { Center, Grid, Text } from '@chakra-ui/react';
+import { Center, Grid, GridItem } from '@chakra-ui/react';
 import { useUserStore } from '~/store/index';
 
 const Layout = dynamic(() => import('~/components/Layout'));
 const Loading = dynamic(() => import('~/components/Loading'));
 const GuildIcon = dynamic(() => import('~/components/GuildIcon'));
+const GuildText = dynamic(() => import('~/components/GuildText'));
 
 const Guilds = () => {
   const user = useUserStore();
@@ -22,16 +23,16 @@ const Guilds = () => {
 
   return (
     <Grid templateColumns = "repeat(auto-fit, 150px)" gap = "32px 0px"
-      placeContent = "center">
+      placeContent = "center" >
       {user.guilds?.map(guild => (
-        <Link href = {`/guilds/${guild.id}`} key = {guild.id}>
-          <Grid gap = "8px 0px" placeItems = "center">
-            <GuildIcon guild = {guild} />
-            <Text textAlign = "center" fontWeight = "semibold">
-              {guild.name}
-            </Text>
-          </Grid>
-        </Link>
+        <GridItem key = {guild.id}>
+          <Link href = {`/guilds/${guild.id}`} >
+            <Grid gap = "8px 0px" placeItems = "center">
+              <GuildIcon guild = {guild} />
+              <GuildText name = {guild.name} />
+            </Grid>
+          </Link>
+        </GridItem>
       ))}
     </Grid>
   );
