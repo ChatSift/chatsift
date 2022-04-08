fbf8b34a9e44b4e76b45f2deb37c7202b862265f
didinele <didinele.dev@gmail.com>
2022-04-08 17:23:53 +0300

    chore: nuke dash

--- services/dash/src/utils/filterEmptyFields.ts
+++
@@ -1,4 +0,0 @@
-export const filterEmptyFields = <T extends Record<string, any>>(obj: T): T =>
-	Object.fromEntries(
-		Object.entries(obj).filter(([, value]) => typeof value !== 'string' || value.length),
-	) as unknown as T;
