14243a09b05bdf5e3b704a6451da7ea74e192aec
didinele <didinele.dev@gmail.com>
2022-04-21 22:34:12 +0300

    feat: case management

--- services/interactions/src/interactions/mod/index.ts
+++ services/interactions/src/interactions/mod/index.ts
@@ -1,6 +1,7 @@
 export * from './context-menus/report-message';
 export * from './context-menus/report-message-reason';
 export * from './ban';
+export * from './case';
 export * from './kick';
 export * from './mute';
 export * from './softban';
