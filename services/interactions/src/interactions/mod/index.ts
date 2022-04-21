0f8d1289f6b59c960effbbbf9a96b57d07d2ef0f
didinele <didinele.dev@gmail.com>
2022-04-21 13:28:00 +0300

    feat: ban command

--- services/interactions/src/interactions/mod/index.ts
+++ services/interactions/src/interactions/mod/index.ts
@@ -1,5 +1,6 @@
 export * from './context-menus/report-message';
 export * from './context-menus/report-message-reason';
+export * from './ban';
 export * from './kick';
 export * from './mute';
 export * from './softban';
