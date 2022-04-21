47ceb0c3f959c3f89d2bb1314d4a2348b44202f6
didinele <didinele.dev@gmail.com>
2022-04-21 13:25:28 +0300

    feat: softban command

--- services/interactions/src/interactions/mod/index.ts
+++ services/interactions/src/interactions/mod/index.ts
@@ -2,4 +2,5 @@ export * from './context-menus/report-message';
 export * from './context-menus/report-message-reason';
 export * from './kick';
 export * from './mute';
+export * from './softban';
 export * from './warn';
