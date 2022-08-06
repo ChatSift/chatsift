9b696055c065e75355d01023a5e30dc299dbec67
didinele <didinele.dev@gmail.com>
2022-08-06 14:54:05 +0300

    refactor: logging

--- packages/logger/src/index.ts
+++ packages/logger/src/index.ts
@@ -1,35 +1,51 @@
 import { join } from 'path';
 import { Config, kConfig } from '@automoderator/injection';
+import type { PinoRotateFileOptions } from '@chatsift/pino-rotate-file';
 import createLogger, { multistream, transport } from 'pino';
-import pinoPretty from 'pino-pretty';
+import type { PrettyOptions } from 'pino-pretty';
 import { container } from 'tsyringe';
 
 export default (service: string) => {
 	const { nodeEnv } = container.resolve<Config>(kConfig);
+	const level = nodeEnv === 'prod' ? 'debug' : 'trace';
+
+	const pinoPrettyOptions: PrettyOptions = {
+		colorize: true,
+		levelFirst: true,
+		translateTime: true,
+	};
+
+	const pinoRotateFileOptions: PinoRotateFileOptions = {
+		dir: join(process.cwd(), 'logs', service.toLowerCase()),
+		mkdir: true,
+		maxAgeDays: 14,
+		prettyOptions: {
+			...pinoPrettyOptions,
+			colorize: false,
+		},
+	};
+
 	return createLogger(
 		{
 			name: service.toUpperCase(),
-			level: nodeEnv === 'prod' ? 'debug' : 'trace',
+			level,
 		},
 		multistream([
 			{
-				stream: pinoPretty({
-					colorize: true,
-					levelFirst: true,
-					translateTime: true,
+				// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+				stream: transport({
+					target: 'pino-pretty',
+					options: pinoPrettyOptions,
 				}),
-				level: 'debug',
+				level,
 			},
 			{
 				// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 				stream: transport({
-					target: 'pino/file',
-					options: {
-						destination: join(process.cwd(), 'logs', `${service}.log`),
-						mkdir: true,
-					},
+					target: '@chatsift/pino-rotate-file',
+					options: pinoRotateFileOptions,
 				}),
-				level: 'debug',
+				level,
 			},
 		]),
 	);
