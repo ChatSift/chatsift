564152846e824b19414305af1525ce595b492434
didinele <didinele.dev@gmail.com>
2023-09-01 12:01:06 +0300

    refactor: remove result usage

--- packages/core/src/struct/Util.ts
+++ packages/core/src/struct/Util.ts
@@ -1,47 +1,27 @@
 import { API } from '@discordjs/core';
-import { Result } from '@sapphire/result';
 import type { APIUser } from 'discord-api-types/v10';
 import { inject, injectable } from 'inversify';
-
-export enum DMUserFailableSteps {
-	MemberCheck,
-	ChannelCreation,
-	Message,
-}
-
-export interface DMUserError {
-	exception?: unknown;
-	step: DMUserFailableSteps;
-}
-
-export type DMUserResult = Result<void, DMUserError>;
 
 @injectable()
 export class Util {
 	@inject(API)
 	private readonly api!: API;
 
-	public async dmUser(userId: string, content: string, guildId?: string): Promise<DMUserResult> {
+	public async tryDmUser(userId: string, content: string, guildId?: string): Promise<boolean> {
 		if (guildId) {
 			const member = await this.api.guilds.getMember(guildId, userId).catch(() => null);
 			if (!member) {
-				return Result.err<DMUserError>({ step: DMUserFailableSteps.MemberCheck });
+				return false;
 			}
 		}
 
-		const dmChannelResult = await Result.fromAsync(async () => {
-			return this.api.users.createDM(userId);
-		});
-
-		if (dmChannelResult.isErr()) {
-			return dmChannelResult.mapErr(this.wrapDmUserException(DMUserFailableSteps.ChannelCreation));
+		const dmChannel = await this.api.users.createDM(userId).catch(() => null);
+		if (!dmChannel) {
+			return false;
 		}
 
-		const result = await Result.fromAsync(async () => {
-			await this.api.channels.createMessage(dmChannelResult.unwrap().id, { content });
-		});
-
-		return result.mapErr(this.wrapDmUserException(DMUserFailableSteps.Message));
+		const message = await this.api.channels.createMessage(dmChannel.id, { content }).catch(() => null);
+		return Boolean(message);
 	}
 
 	public getUserTag(user?: APIUser | null): string {
@@ -60,14 +40,5 @@ export class Util {
 		return user.avatar
 			? this.api.rest.cdn.avatar(user.id, user.avatar, { extension: 'webp' })
 			: this.api.rest.cdn.defaultAvatar(Number.parseInt(user.discriminator, 10) % 5);
-	}
-
-	private wrapDmUserException(step: DMUserFailableSteps) {
-		return (exception: unknown): DMUserError => {
-			return {
-				step,
-				exception,
-			};
-		};
 	}
 }
