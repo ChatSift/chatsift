e190e5498e261a4dc719db7b66befcc0bed55d1d
didinele <didinele.dev@gmail.com>
2024-07-31 13:32:28 +0300

    chore: improve home page mobile responsiveness

--- apps/website/src/app/page.tsx
+++ apps/website/src/app/page.tsx
@@ -2,19 +2,21 @@ import { ExternalLink } from 'lucide-react';
 
 export default function Home() {
 	return (
-		<main className="flex flex-col max-w-screen-md m-auto gap-3 pt-6">
-			<h1 className="text-3xl font-semibold">Modern solutions for your Discord communities</h1>
+		<div className="flex flex-col">
+			<main className="flex flex-col justify-center max-w-[80vw] md:min-w-[912px] m-auto gap-6 pt-6">
+				<h1 className="text-3xl font-semibold">Modern solutions for your Discord communities</h1>
 
-			<div>
-				<a
-					className="inline-flex gap-2 bg-misc-accent rounded-md px-4 py-3 font-medium text-lg"
-					href="/support"
-					rel="noopener noreferrer"
-					target="_blank"
-				>
-					Join our Discord server <ExternalLink aria-hidden size={20} />
-				</a>
-			</div>
-		</main>
+				<div>
+					<a
+						className="md:inline-flex flex justify-center gap-2 bg-misc-accent rounded-md px-4 py-3 font-medium text-lg"
+						href="/support"
+						rel="noopener noreferrer"
+						target="_blank"
+					>
+						Join our Discord server <ExternalLink aria-hidden size={20} />
+					</a>
+				</div>
+			</main>
+		</div>
 	);
 }
