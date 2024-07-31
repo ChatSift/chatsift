a7def733eabac0725cd94ed9504e55a45280abfe
didinele <didinele.dev@gmail.com>
2024-07-31 10:29:58 +0300

    feat(website): head

--- apps/website/src/app/layout.tsx
+++ apps/website/src/app/layout.tsx
@@ -1,8 +1,19 @@
+import type { Metadata } from 'next';
 import type { PropsWithChildren } from 'react';
 import { Providers } from './providers';
 import Navbar from '~/components/Navbar';
 
 import '~/styles/globals.css';
+
+export const metadata: Metadata = {
+	title: {
+		template: '%s | ChatSift',
+		default: 'ChatSift',
+	},
+	icons: {
+		other: [{ rel: 'icon', url: '/assets/favicon.ico' }],
+	},
+};
 
 export default async function RootLayout({ children }: PropsWithChildren) {
 	return (
