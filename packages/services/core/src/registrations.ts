3038f784345df05d5a6d1bc6638c4a7d90321a68
didinele <didinele.dev@gmail.com>
2024-08-04 22:04:07 +0300

    fix: don't register command handler in lib

--- packages/services/core/src/registrations.ts
+++ packages/services/core/src/registrations.ts
@@ -1,11 +1,8 @@
-import { CoralCommandHandler } from './command-framework/CoralCommandHandler.js';
-import { ICommandHandler } from './command-framework/ICommandHandler.js';
 import { globalContainer } from './container.js';
 import { IDatabase } from './database/IDatabase.js';
 import { KyselyPostgresDatabase } from './database/KyselyPostgresDatabase.js';
 import { ExperimentHandler } from './experiments/ExperimentHandler.js';
 import { IExperimentHandler } from './experiments/IExperimentHandler.js';
 
-globalContainer.bind<ICommandHandler<unknown>>(ICommandHandler).to(CoralCommandHandler);
 globalContainer.bind<IDatabase>(IDatabase).to(KyselyPostgresDatabase);
 globalContainer.bind<IExperimentHandler>(IExperimentHandler).to(ExperimentHandler);
