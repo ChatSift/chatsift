37d7d733b2ac60a66729ccda4a442a537257973d
didinele <didinele.dev@gmail.com>
2024-08-04 14:13:09 +0300

    refactor: use root layout

--- apps/website/src/app/layout.tsx
+++ apps/website/src/app/layout.tsx
@@ -1,6 +1,12 @@
+import { HydrationBoundary } from '@tanstack/react-query';
 import type { Metadata } from 'next';
 import type { PropsWithChildren } from 'react';
 import { Providers } from '~/app/providers';
+import { Toaster } from '~/components/common/Toaster';
+import Footer from '~/components/footer/Footer';
+import Navbar from '~/components/header/Navbar';
+import { prefetchUserMe } from '~/data/userMe/server';
+import { cn } from '~/util/util';
 
 import '~/styles/globals.css';
 
@@ -14,11 +20,28 @@ export const metadata: Metadata = {
 	},
 };
 
-export default function RootLayout({ children }: PropsWithChildren) {
+export default async function RootLayout({ children }: PropsWithChildren) {
 	return (
 		<html lang="en" suppressHydrationWarning>
 			<body className="bg-base dark:bg-base-dark">
-				<Providers>{children}</Providers>
+				<Providers>
+					<HydrationBoundary state={await prefetchUserMe()}>
+						<div className="h-scren flex min-h-screen flex-col">
+							<Navbar />
+							<div className="flex flex-[1_1_auto] flex-grow flex-col gap-8">
+								<main
+									className={cn(
+										'mx-auto mb-auto flex max-w-[80vw] flex-col justify-center gap-6 pt-6 md:min-w-[912px]',
+									)}
+								>
+									{children}
+								</main>
+								<Footer />
+							</div>
+						</div>
+						<Toaster />
+					</HydrationBoundary>
+				</Providers>
 			</body>
 		</html>
 	);
