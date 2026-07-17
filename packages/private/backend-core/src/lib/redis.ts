a465c9d1602c2519e21c6239326fb7fdc5792556
didinele <didinele.dev@gmail.com>
2026-07-17 14:10:40 +0300

    chore: redis url env var

--- packages/private/backend-core/src/lib/redis.ts
+++ packages/private/backend-core/src/lib/redis.ts
@@ -1,9 +1,10 @@
 import { Buffer } from 'node:buffer';
 import type { Logger } from 'pino';
 import { createClient, RESP_TYPES } from 'redis';
+import { ENV } from './env.js';
 
 export async function createRedis(logger: Logger) {
-	return createClient({ url: 'redis://redis:6379' })
+	return createClient({ url: ENV.REDIS_URL })
 		.withTypeMapping({
 			[RESP_TYPES.BLOB_STRING]: Buffer,
 		})
