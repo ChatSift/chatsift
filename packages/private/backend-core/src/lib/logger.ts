29293b2959b64f308e140ae8436004ac71eea5dc
didinele <didinele.dev@gmail.com>
2026-07-19 09:41:29 +0300

    feat(logger): pretty logs in dev

--- packages/private/backend-core/src/lib/logger.ts
+++ packages/private/backend-core/src/lib/logger.ts
@@ -1,18 +1,29 @@
 import type { TransportTargetOptions } from 'pino';
 import { pino as createPinoLogger, transport as pinoTransport, stdTimeFunctions } from 'pino';
+import { ENV } from './env.js';
 
 export type { Logger } from 'pino';
 
 export function createLogger(name: string) {
 	// TODO: File rotations for prod?
 	const targets: TransportTargetOptions[] = [
-		{
-			target: 'pino/file',
-			level: 'trace',
-			options: {
-				destination: 1, // stdout
-			},
-		},
+		ENV.IS_PRODUCTION
+			? {
+					target: 'pino/file',
+					level: 'trace',
+					options: {
+						destination: 1, // stdout
+					},
+				}
+			: {
+					target: 'pino-pretty',
+					level: 'trace',
+					options: {
+						destination: 1, // stdout
+						colorize: true,
+						translateTime: 'SYS:standard',
+					},
+				},
 	];
 
 	const transport = pinoTransport({
